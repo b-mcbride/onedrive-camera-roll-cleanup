01859ee9ddd2823fa6fafa00492e3860be6f2053
Brian McBride <brian.mcbride@outlook.com>
2019-01-11 22:09:49 -0800

    Removing oauth.ts

--- src/oauth.ts
+++
@@ -1,8 +0,0 @@
-export const OAuthSettings = {
-    appId: '8e9ea2b9-a755-4bc9-a046-d8503f41f074',
-    scopes: [
-        'user.read',
-        'calendars.read',
-        'files.readwrite.all'
-    ]
-};
